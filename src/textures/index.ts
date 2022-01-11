c365317ce1924b8c56968660b61ec1ef914ea7cb
Travis Craft <tlcraft@users.noreply.github.com>
2022-01-11 13:57:07 -0700

    Add texture index.

---
+++ src/textures/index.ts
@@ -0,0 +1,17 @@
+import door from './Door_Wood_001_basecolor.jpg';
+import doorAmbientOcclusion from './Door_Wood_001_ambientOcclusion.jpg';
+import doorHeight from './Door_Wood_001_height.png';
+import doorMetallic from './Door_Wood_001_metallic.jpg';
+import doorNormal from './Door_Wood_001_normal.jpg';
+import doorOpacity from './Door_Wood_001_opacity.jpg';
+import doorRoughness from './Door_Wood_001_roughness.jpg';
+
+export {
+    door,
+    doorAmbientOcclusion,
+    doorHeight,
+    doorMetallic,
+    doorNormal,
+    doorOpacity,
+    doorRoughness
+}
