8d45882439b0b6b9633ee6d20b98c40d11f50bd9
Travis Craft <tlcraft@users.noreply.github.com>
2022-01-14 14:41:03 -0700

    Add uv2 attribute.

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import {
   ConeBufferGeometry,
   DoubleSide,
   EdgesGeometry,
+  Float32BufferAttribute,
   Fog,
   Group,
   Light,
@@ -267,6 +268,7 @@ function generateDoor(): Mesh {
      });
     const door = new Mesh(geometry, material);
     door.position.set(4.85, -2, 2.01);
+    door.geometry.setAttribute("uv2", new Float32BufferAttribute(door.geometry.attributes.uv.array, 2))
     return door; 
 }
 
