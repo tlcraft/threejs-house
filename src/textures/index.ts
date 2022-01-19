3d08ab1ddeaec233e447401d92dffb636e54b287
Travis Craft <tlcraft@users.noreply.github.com>
2022-01-19 14:38:11 -0700

    Add brick textures to index.

--- src/textures/index.ts
+++ src/textures/index.ts
@@ -1,3 +1,8 @@
+import brickAmbientOcclusion from './bricks/Brick_Wall_019_ambientOcclusion.jpg'
+import brickBaseColor from './bricks/Brick_Wall_019_basecolor.jpg'
+import brickHeight from './bricks/Brick_Wall_019_height.png'
+import brickNormal from './bricks/Brick_Wall_019_normal.jpg'
+import brickRoughness from './bricks/Brick_Wall_019_roughness.jpg'
 import door from './door/Door_Wood_001_basecolor.jpg';
 import doorAmbientOcclusion from './door/Door_Wood_001_ambientOcclusion.jpg';
 import doorHeight from './door/Door_Wood_001_height.png';
@@ -7,6 +12,11 @@ import doorOpacity from './door/Door_Wood_001_opacity.jpg';
 import doorRoughness from './door/Door_Wood_001_roughness.jpg';
 
 export {
+    brickAmbientOcclusion,
+    brickBaseColor,
+    brickHeight,
+    brickNormal,
+    brickRoughness,
     door,
     doorAmbientOcclusion,
     doorHeight,
