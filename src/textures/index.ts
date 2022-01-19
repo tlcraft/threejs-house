32feffde36cf26dd52c29d4b376063d675f1f756
Travis Craft <tlcraft@users.noreply.github.com>
2022-01-18 18:18:22 -0700

    Move door images.

--- src/textures/index.ts
+++ src/textures/index.ts
@@ -1,10 +1,10 @@
-import door from './Door_Wood_001_basecolor.jpg';
-import doorAmbientOcclusion from './Door_Wood_001_ambientOcclusion.jpg';
-import doorHeight from './Door_Wood_001_height.png';
-import doorMetallic from './Door_Wood_001_metallic.jpg';
-import doorNormal from './Door_Wood_001_normal.jpg';
-import doorOpacity from './Door_Wood_001_opacity.jpg';
-import doorRoughness from './Door_Wood_001_roughness.jpg';
+import door from './door/Door_Wood_001_basecolor.jpg';
+import doorAmbientOcclusion from './door/Door_Wood_001_ambientOcclusion.jpg';
+import doorHeight from './door/Door_Wood_001_height.png';
+import doorMetallic from './door/Door_Wood_001_metallic.jpg';
+import doorNormal from './door/Door_Wood_001_normal.jpg';
+import doorOpacity from './door/Door_Wood_001_opacity.jpg';
+import doorRoughness from './door/Door_Wood_001_roughness.jpg';
 
 export {
     door,
